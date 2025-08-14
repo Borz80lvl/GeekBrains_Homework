7fa311d80be72cefa5604cee410e90341e5b96ff
Borz80lvl <nofaki@bk.ru>
2025-08-14 14:03:32 +0300

    added all new outputs

--- Java/OOP/Printer.java
+++ Java/OOP/Printer.java
@@ -1,13 +1,16 @@
-import task2.*;
+import model.FamilyTree;
+import model.Person;
+import service.*;
 
+import java.util.Iterator;
 
 public class Printer {
 
     public static void main(String[] args) {
         //Task 1
         FamilyTree familyTree = new FamilyTree();
-        Person john = new Person("John", new int[]{5, 6, 2000});
-        Person emma = new Person("Emma", new int[]{2, 12, 1996});
+        Person john = new Person("John", new int[]{5, 6, 1996});
+        Person emma = new Person("Emma", new int[]{2, 12, 2000});
         Person isabella = new Person("Isabella", new int[]{2, 12, 2010});
         isabella.setMother(emma.getName());
         isabella.setFather(john.getName());
@@ -23,5 +26,15 @@ public class Printer {
         isabella.getFromFile();
         familyTree.saveToFile();
         familyTree.getFromFile();
+
+        //Task 3
+        Iterator<Person> familyIterator = familyTree.iterator();
+        while (familyIterator.hasNext()) System.out.println(familyIterator.next());
+        System.out.println("Сортировка по имени:\n");
+        System.out.printf("Before: %s\n", familyTree);
+        System.out.printf("After: %s\n\n", familyTree.sortByName());
+        System.out.println("Сортировка по году рождения:\n");
+        System.out.printf("Before: %s\n", familyTree);
+        System.out.printf("After %s\n", familyTree.sortByBirthYear());
     }
 }
