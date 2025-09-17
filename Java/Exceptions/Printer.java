b0cdf71c82863f2ef7e12e8fda79b2e085527455
Borz80lvl <nofaki@bk.ru>
2025-09-17 18:16:43 +0300

    added task2 outputs for print

--- Java/Exceptions/Printer.java
+++ Java/Exceptions/Printer.java
@@ -4,28 +4,51 @@ public class Printer {
     public static void main(String[] args) {
         // Task 1
         // 1
-        String[] correctStrings = new String[] {"59", "15", "4", "10", "12"};
-        String[] incorrectStrings = new String[] {"22", "%", "ab", "10", "122"};
-        String[] correctNotLessLimitStrings = new String[] {"22", "2", "4", "10", "122"};
+        String[] correctStrings = new String[]{"59", "15", "4", "10", "12"};
+        String[] incorrectStrings = new String[]{"22", "%", "ab", "10", "122"};
+        String[] correctNotLessLimitStrings = new String[]{"22", "2", "4", "10", "122"};
         System.out.println(task1.convertAndSum(correctStrings));
         System.out.println(task1.convertAndSum(incorrectStrings));
         System.out.println(task1.convertAndSum(correctNotLessLimitStrings));
         // 2
-        int[] correctArrayX = new int[] {1, 3, 5};
-        int[] correctArrayY = new int[] {2, 4, 6};
-        int[] longerArrayX = new int[] {1, 3, 5, 7, 9};
-        int[] incorrectArrayX = new int[] {1, -3, 5};
+        int[] correctArrayX = new int[]{1, 3, 5};
+        int[] correctArrayY = new int[]{2, 4, 6};
+        int[] longerArrayX = new int[]{1, 3, 5, 7, 9};
+        int[] incorrectArrayX = new int[]{1, -3, 5};
         System.out.println(Arrays.toString(task1.mergeAndValidateArrays(correctArrayX, correctArrayY)));
         System.out.println(Arrays.toString(task1.mergeAndValidateArrays(longerArrayX, correctArrayY)));
         System.out.println(Arrays.toString(task1.mergeAndValidateArrays(incorrectArrayX, correctArrayY)));
         // 3
-        correctArrayX = new int[] {5, 6, 8};
+        correctArrayX = new int[]{5, 6, 8};
         System.out.println(Arrays.toString(task1.subArraysWithExceptionHandling(correctArrayX, correctArrayY)));
         System.out.println(Arrays.toString(task1.subArraysWithExceptionHandling(longerArrayX, correctArrayY)));
         System.out.println(Arrays.toString(task1.subArraysWithExceptionHandling(correctArrayY, correctArrayX)));
         // 4
-        String[] worlds = new String[] {"Hello ", "Python ","!"};
-        System.out.println(Arrays.toString(task1.findAndReplace(worlds,"Python ","World ")));
+        String[] worlds = new String[]{"Hello ", "Python ", "!"};
+        System.out.println(Arrays.toString(task1.findAndReplace(worlds, "Python ", "World ")));
         System.out.println(Arrays.toString(task1.findAndReplace(worlds, "cat", "rat")));
+
+        // Task 2
+        // 1
+        System.out.println(task2.validateDate("2025-09-14"));
+        System.out.println(task2.validateDate("0-03-13"));
+        System.out.println(task2.validateDate("2000-22-03"));
+        System.out.println(task2.validateDate("2020-05-55"));
+        System.out.println(task2.validateDate("2025-02-31"));
+        System.out.println(task2.validateDate("-"));
+        System.out.println(task2.validateDate("YYYY-MM-DD"));
+        // 2
+        System.out.println(task2.isPalindrome("1113"));
+        System.out.println(task2.isPalindrome("66659++95666"));
+        System.out.println(task2.isPalindrome("aababbadbbba"));
+        System.out.println(task2.isPalindrome("%$%$$$^*&^%%%"));
+        System.out.println(task2.isPalindrome("+-ddba$$abdd-+"));
+        // 3
+        worlds = new String[] {"cinema", "peak", "absolute"};
+        System.out.println(Arrays.toString(task2.sortStrings(worlds)));
+        System.out.println(Arrays.toString(task2.sortStrings(null)));
+        // 4
+        System.out.println(task2.findAverage(longerArrayX));
+        System.out.println(task2.findAverage(null));
     }
 }
